8fd5bb86d03416fb5d079a32c604701bd58d02b8
cetinklima80-star <cetinklima80@gmail.com>
2026-03-04 02:42:18 +0300

    auto update

--- js/okuma.js
+++ js/okuma.js
@@ -30,3 +30,53 @@ function decreaseFont(){
 function toggleDark(){
   document.body.classList.toggle("light-mode");
 }
+const readerText = document.getElementById("readerText");
+
+function loadText(text) {
+  const words = text.split(" ");
+  readerText.innerHTML = "";
+
+  words.forEach(word => {
+    const span = document.createElement("span");
+    span.textContent = word + " ";
+    span.classList.add("clickable-word");
+
+    span.onclick = async () => {
+      showMeaning(span, word);
+    };
+
+    readerText.appendChild(span);
+  });
+}
+
+async function showMeaning(element, word) {
+  const cleanWord = word.replace(/[.,!?]/g, "");
+
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=de&tl=tr&dt=t&q=${encodeURIComponent(cleanWord)}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const meaning = data[0][0][0];
+
+    showPopup(element, meaning);
+  } catch (error) {
+    console.log("Çeviri hatası:", error);
+  }
+}
+
+function showPopup(element, meaning) {
+  let popup = document.createElement("div");
+  popup.classList.add("word-popup");
+  popup.textContent = meaning;
+
+  document.body.appendChild(popup);
+
+  const rect = element.getBoundingClientRect();
+  popup.style.top = rect.bottom + window.scrollY + "px";
+  popup.style.left = rect.left + window.scrollX + "px";
+
+  setTimeout(() => {
+    popup.remove();
+  }, 3000);
+}
