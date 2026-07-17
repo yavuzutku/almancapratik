9a23f5ddf54406a8cab9b7eb44114174b36b4391
cetinklima80-star <cetinklima80@gmail.com>
2026-07-17 14:34:04 +0300

    auto update

--- auto_push.sh
+++ auto_push.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# İlk olarak sitemap oluşturma betiğini çalıştırıyoruz
+python3 sitemap_olustur.py
+
+# Değişiklikleri sahneye ekliyoruz (yeni sitemap dahil)
 git add .
+
+# Değişiklikleri kaydediyoruz
 git commit -m "auto update"
+
+# GitHub'a yüklüyoruz
 git push
